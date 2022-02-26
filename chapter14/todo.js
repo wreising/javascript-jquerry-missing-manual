16920f7f7941919d0a56897befb4c066d751fd66
wreising <wreising@mac.com>
2022-02-26 05:46:39 -0800

    m

--- chapter14/todo.js
+++ chapter14/todo.js
@@ -1,3 +1,24 @@
-$(document).ready(function(e) {
+$(document).ready(function (e) {
+
+  $('#add-todo').button({
+    icons: {
+      primary: "ui-icon-circle-plus"
+    }
+  }).click(function () {
+    $('#new-todo').dialog('open')
+  })
+
+  $('#new-todo').dialog({
+    modal: true,
+    autoOpen: false,
+    buttons: {
+      "Add task": function () {
+
+      },
+      "Cancel": function () {
+        $(this).dialog('close')
+      }
+    }
+  })
 
 }); // end ready
